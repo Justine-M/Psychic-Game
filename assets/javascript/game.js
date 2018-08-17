932af9917c81a70ecf354413e1e3fbf6debfd128
Justine-M <justalexmays@hotmail.com>
2018-08-16 22:51:40 -0400

    first draft of JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,32 +2,58 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q","r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+
 var wins = 0;
 var losses = 0;
-var guessLeft = 0;
-var guessSoFar = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
 
  // This function is run whenever the user presses a key.
  document.onkeyup = function(event) {
      // Determines which key was pressed.
      var userGuess = event.key;
+     
 
-     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+     var computerLetters = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
      // Randomly chooses a choice from the options array. This is the Computer's guess.
-    if (userGuess === computerGuess) {
+    if (userGuess === computerLetters) {
         wins++;
-      }
+		console.log("You won!");
+		guessesLeft = 9;
+		guessesSoFar = [];
+		computerLetters = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + computerGuess);
+	}
+
+     
+    if (guessesSoFar.indexOf(userGuess) < 0 && computerChoices.indexOf(userGuess) >= 0) {
+		guessesSoFar[guessesSoFar.length]=userGuess;
+		// if it is a new letter then decrease remaining guesses by 1
+		guessesLeft--;
+    }
+    
+    if (guessesLeft == 0) {
+		losses++;
+		console.log("You lost!");
+		guessesLeft = 9;
+		guessesSoFar = [];
+		computerLetters = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + computerGuess);
+	}
+
 
     
 
      // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
      var html =
      "<p>Wins: " + wins + "</p>" +
-     "<p>losses: " + losses + "</p>";
+     "<p>Losses: " + losses + "</p>" +
+     "<p>Guesses Left: " + guessesLeft + "</p>" +
+     "<p>Guesses So Far: " + guessesSoFar + "</p>";
+
     
 
    // Set the inner HTML contents of the #game div to our html string
    document.querySelector("#psychicGame").innerHTML = html;
-
-}
+ }
